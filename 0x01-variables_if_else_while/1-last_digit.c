d2a296f997cf1d98ed30327a057bbe9bd960c600
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-02-08 18:11:22 -0500

    made betty compliant

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,21 +10,27 @@
 
 int main(void)
 {
-  int n;
+	int n;
 
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  printf("Last digit of %i is", n);
-  int last = n % 10;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %i is", n);
+	int last = n % 10;
 
-  if (last > 5)
-    printf(" %i and is greater then 5\n", last);
+	if (last > 5)
+	{
+		printf(" %i and is greater then 5\n", last);
+	}
 
-  else if (last == 0)
-    printf(" %i and is 0\n", last);
+	else if (last == 0)
+	{
+		printf(" %i and is 0\n", last);
+	}
 
-  else if (last < 6)
-    printf(" %i and is less than 6\n", last);
+	else if (last < 6)
+	{
+		printf(" %i and is less than 6\n", last);
+	}
 
-  return (0);
+	return (0);
 }
