17ca0622ba269d9964861d266501c0167262f4b1
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-02-08 21:29:20 -0500

    alphabet no e&q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		if (ch == 'q' || ch == 'e')
+		{
+			ch++;
+		}
+		else
+			putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
