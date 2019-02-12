bca7c180c4d670e28d3f5bd78ba177e31aaa650f
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-02-12 14:05:51 -0500

    x10 prints alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
- *   main - Entry point
+ *   print_alphabet_x10 - its pretty much in the name
  *
- *  Return: Always 0 (Success)
+ *  Return: void
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int n;
 	char ch;
 
-	for (n == 0; n < 10; n++)
+	for (n = 0; n < 10; n++)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 		{
-			putchar(ch);
+			_putchar(ch);
 		}
-		putchar ('\n');
+		_putchar ('\n');
 	}
 }
