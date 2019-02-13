403970448125776382aa932cd5fd7155bfbda716
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-02-12 19:09:12 -0500

    positive or negitive

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * print_sign - checks if n is positive or negitive 
+ *@n: is the number im checking
+ * Return: 1 if + , 0 if 0, and -1 if -
+ */
+int print_sign(int n)
+{
+	int i = 0;
+
+	if (n > 0)
+	{
+		i = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		i = 0;
+		_putchar('0');
+	}
+	else
+	{
+		i = -1;
+		_putchar('-');
+	}
+
+	return (i);
+	_putchar('\n');
+}
