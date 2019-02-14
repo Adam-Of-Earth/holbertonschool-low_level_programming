37eaa18a95a1b6a3b7a4daff314065ccf9880e9f
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-02-14 16:18:08 -0500

    counts to 14 ten times

--- 0x03-more_functions_nested_loops/5-more_numbers.c
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -14,15 +14,14 @@ void more_numbers(void)
 
 		for (n = 0; n <= 14; n++)
 		{
-			if (n <= 9)
-			{
-				_putchar(n + '0');
-			}
-			else
+
+			while (n > 9)
 			{
 				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
+				break;
 			}
+			_putchar((n % 10) + '0');
+
 		}
 		_putchar('\n');
 	}
---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * print_diagonal - make a line at an angle
+ *@n: line langth
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int space = n;
+
+	for (; n >= 0; 
