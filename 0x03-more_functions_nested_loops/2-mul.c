12fe69bfa32def2de01d62cb3de94868e06f02f9
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-02-14 13:01:52 -0500

    finds a product

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * mul - mutiplys two integers
+ *@a: first int
+ *@b: second int
+ * Return: the product
+ */
+int mul(int a, int b)
+{
+	int sum = a * b;
+
+	return (sum);
+}
