9fc00e8179777932411a66d2276c2a3081ec191e
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-02-14 13:44:08 -0500

    prints most numbers

---
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - prints 0-9 without 2 or 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	for (; i <= 9; i++)
+	{
+		if (i == 2 || 1 == 4)
+			i = i + 0;
+		else
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
