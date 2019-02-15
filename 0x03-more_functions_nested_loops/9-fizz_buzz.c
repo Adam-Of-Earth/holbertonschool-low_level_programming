4ad4c9398b0aefffea324ecc39c3e692cf70a545
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-02-14 23:20:58 -0500

    FizzBuzz test

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * Fizz_Buzz - counts to 100 replacing 3 and 5s with Fizz Buzz
+ *
+ * Return - 0 (success)
+ */
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 1; x <= 100; x++)
+	{
+		y = x % 3;
+		z = x % 5;
+		if ((y == 0) && (z == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (z == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (y == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%i ", x);
+		}
+	}
+	return (0);
+}
