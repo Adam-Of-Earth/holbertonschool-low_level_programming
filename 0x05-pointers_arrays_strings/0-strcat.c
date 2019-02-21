72f10bcfc4872c7fd84d080d48e083b16e0cfca7
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-02-21 14:50:31 -0500

    directory struct and 0

---
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * *_strcat - this program will append two strings
+ * @src: first string
+ * @dest: second string
+ * Return: pointer to new string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int x, y;
+
+	for (x = 0; dest [x]; x++)
+	{
+	}
+	y = 0;
+	while (src[y] != 0)
+	{
+		dest[x + y] = src[y];
+		y++;
+	}
+	return (dest);
+}
