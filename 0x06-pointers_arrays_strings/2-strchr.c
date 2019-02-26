1d518a7bd612be0f53c78273068194d0fa150b23
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-02-26 13:33:58 -0500

    finds a char

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _strchr - scans through string s to find char c.
+ * @s: string imput
+ * @c: char surching though the string to find
+ * Return: pointer to c in string s or null if not found
+ */
+char *_strchr(char *s, char c)
+{
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		if (c == '\0')
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (s);
+}
