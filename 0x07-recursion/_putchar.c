0cae12756c2a5cd93314a581f5596f0b684585dd
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-02-27 12:36:33 -0500

    dir stuct

---
+++ 0x07-recursion/_putchar.c
@@ -0,0 +1,10 @@
+#include <unistd>
+/**
+ * _putchar - writes the character c to stdout
+ *@c: the character to print
+ *Return: 1 On success -1 on error
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
