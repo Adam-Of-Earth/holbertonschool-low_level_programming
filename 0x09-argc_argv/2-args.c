30aea33d3b67774f0ede92e64c83bed2ddb8f3eb
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-03-03 16:36:27 -0500

    prints all arguments

---
+++ 0x09-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - prints all its arguments
+ * @argc: number of arguments
+ * @argv: what is going to print
+ * Return: 0 sucses
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
