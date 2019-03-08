fee37567e80e1d6c5752fc50c263be7984d1e16c
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-03-07 19:19:33 -0500

    concat two strings

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -23,6 +23,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len = len + (n + 1);
 	bot = malloc(sizeof(char) * len);
 	counter = bot;
+	if (bot == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; s1[i]; i++)
 	{
 		counter[i] = s1[i];
