8c0a4ec9e9dfc907588bd296deac52051e0ceadf
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-03-22 14:52:18 -0400

    struct and 0

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -1,6 +1,7 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
+
 /**
  * print_all - prints char, int, float, and strings
  * @format: type of argument passed
@@ -9,4 +10,55 @@
 void print_all(const char * const format, ...)
 {
 	va_list valist;
-	
+	int i = 0, n = 0;
+	char *s1 = "", *s2 = ", ";
+
+	types print_types[] = {
+		{'c', op_c},
+		{'f', op_f},
+		{'i', op_i},
+		{'s', op_s},
+		{'\0', NULL}
+	};
+	va_start(valist, format);
+	while (format != NULL && format[i] != '\0')
+	{
+		while (oops[n].c != NULL)
+		{
+			if (oops[n].c == format[i])
+			{
+				printf("%s", s1);
+				ops[i].ptr(valist);
+				s1 = s2;
+			}
+			n++;
+		}
+		i++;
+	}
+	putchar('\n');
+	va_end(valist);
+}
+
+void op_c(va_list valist)
+{
+	printf("%c", va_arg(valist, int));
+}
+void op_i(va_list valist)
+{
+	printf("%i", va_arg(valist, int));
+}
+void op_f(va_list valist)
+{
+	printf("%f", va_arg(valist, double));
+}
+void op_s(va_list valist)
+{
+	char *a = va_arg(valist, char *);
+
+	if (a == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", a);
+}
---
+++ 0x11-singly_linked_list/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		if (h->str != NULL)
+		{
+			printf("[%u]%s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0](nil)\n");
+		}
+		h = h->next;
+	}
+	return(i);
+}
