7ce9ec397ee2c5886fe266c29d0f9f1358e7f315
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-04-18 18:42:49 -0400

    push

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,30 +9,24 @@
  */
 int main(void)
 {
-	int n;
+	int n, last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("Last digit of %i is", n);
-	int last = n % 10;
+	last = n % 10;
+	printf("Last digit of %d is %d and is ", n, last);
 
-	if (last < 0)
-	{
-		last = last * -1;
-	}
 	if (last > 5)
 	{
-		printf(" %i and is greater than 5\n", last);
+		printf("greater than 5\n");
 	}
-
 	else if (last == 0)
 	{
-		printf(" %i and is 0\n", last);
+		printf("0\n");
 	}
-
 	else if (last < 6)
 	{
-		printf(" %i and is less than 6 and not 0\n", last);
+		printf("less than 6 and not 0\n");
 	}
 
 	return (0);
