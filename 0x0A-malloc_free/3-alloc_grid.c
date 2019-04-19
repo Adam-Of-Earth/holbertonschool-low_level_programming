f5fb8f6dd5cd6b80388df448f08a268a766a0c30
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-04-19 14:12:32 -0400

    push

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include "holbeton.h"
+
+/**
+ * alloc_grid - makes space for 2d array
+ * @width: how wide the grid will be
+ * @height: how tall
+ *
+ * Return: array or NULL if falure
+ */
+int **alloc_grid(int width, int height)
+{
+	int **ret;
+	int x, y;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+	ret = malloc(height * sizeof(int *));
+	if (ret == NULL)
+		return (NULL);
+	for (y = 0; y < height; y++)
+	{
+		ret[y] = malloc(width * sizeof(int));
+		if (ret[y] == NULL)
+		{
+			for (height = 0; height < y; height++)
+				free(ret[height]);
+			free(ret);
+			return (NULL);
+		}
+		for (x = 0; x < width; x++)
+			ret[y][x] = 0;
+	}
+	return (ret);
+}
