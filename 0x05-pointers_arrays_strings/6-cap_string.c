12cf29f04820cd1b04b62b8466ebacc946c7d8a5
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-04-19 12:02:14 -0400

    cap_string

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -6,4 +6,25 @@
  */
 char *cap_string(char *s)
 {
-	
+	char *delim = " \t\n,:.!?\"(){}";
+	int index = 0, index2 = 0;
+
+	while (s[index] != '0')
+	{
+		if (s[0] >= 'a' && s[0] <= 'z')
+		{
+			s[0] -= 32;
+		}
+		while (delim[index2] != '\0')
+		{
+			if (s[index] == delim[index2] && s[index + 1] >= 'a' && s[index + 1] <= 'z')
+			{
+				s[index + 1] -= 32;
+			}
+			index2++;
+		}
+		index++;
+		index2 = 0;
+	}
+	return (s);
+}
