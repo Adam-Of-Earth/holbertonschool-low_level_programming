fb6aaee03c844974053daac5cd66298975fc909d
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-04-18 21:57:10 -0400

    print to 98

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,3 +1,5 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -9,3 +11,6 @@ int print_last_digit(int);
 void jack_bauer(void);
 int add(int, int);
 void times_table(void);
+void print_to_98(int n);
+void print_times_table(int n);
+#endif
