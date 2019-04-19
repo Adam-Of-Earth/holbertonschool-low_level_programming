7f3c1bbfa151155169781a76a4f8042c39e1220c
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-04-18 21:31:14 -0400

    return errors

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,12 +11,13 @@ int main(void)
 
 	for (x = 0; x <= 9; x++)
 	{
-		putchar (x + '0');
-		if (x < 9)
+		putchar(x + '0');
+		if (x != 9)
 		{
 			putchar (',');
 			putchar (' ');
 		}
 	}
 	putchar ('\n');
+	return (0);
 }
