5d2ec3c55f1b6598067d76e6ec3f27accdf8a1ec
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-06-19 20:16:55 -0400

    cocktail sort

--- 0x1B-sorting_algorithms/102-counting_sort.c
+++ 0x1B-sorting_algorithms/102-counting_sort.c
@@ -9,48 +9,47 @@
 void counting_sort(int *array, size_t size)
 {
 	int *countA, *sortA;
-    int idx;
-	size_t x = 0;
+	size_t x = 0, idx;
 
 	for(idx = 0; idx != size; idx++)
-	{	
-        printf("loser");
+	{
+		printf("loser");
 
 		if(array[idx] < array[idx + 1])
-		{	
+		{
 			x = array[idx];
-            printf("//spot A---");
+			printf("//spot A---");
 		}
-    }
-    printf(" I'm out  ");
+	}
+	printf(" I'm out  ");
 	countA = malloc(sizeof(int *) * (size + 1));
-    if (countA = NULL)
-    {
-        printf("spot C");
-        return;
-    }
-    sortA = malloc((size + 1) * sizeof(int *));
-    if (sortA = NULL)
-    {
-        printf("spot D");
-        return;
-    }
-    printf("Mid E");
-    for(idx = 0; array[idx]; idx++)
-    {
-        printf("spot E");
-        ++countA[array[idx]];
-    }
-    for(idx = 1; idx <= x; idx++)
-    {
-        printf("spot F");
-        countA[idx] += countA[idx - 1];
-    }
-    for(idx = 0; array[idx]; idx++)
-    {
-        printf("spot G");
-        sortA[countA[array[idx]] - 1] = array[idx];
-        --countA[array[idx]];
-    }
+	if(countA == NULL)
+	{
+		printf("spot C");
+		return;
+	}
+	sortA = malloc((size + 1) * sizeof(int *));
+	if(sortA == NULL)
+	{
+		printf("spot D");
+		return;
+	}
+	printf("Mid E");
+	for(idx = 0; array[idx]; idx++)
+	{
+		printf("spot E");
+		++countA[array[idx]];
+	}
+	for(idx = 1; idx <= x; idx++)
+	{
+		printf("spot F");
+		countA[idx] += countA[idx - 1];
+	}
+	for(idx = 0; array[idx]; idx++)
+	{
+		printf("spot G");
+		sortA[countA[array[idx]] - 1] = array[idx];
+		--countA[array[idx]];
+	}
 
 }
