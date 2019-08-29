255811b6995baf1e480742a057dff2271d8c17fe
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-08-29 19:48:05 -0400

    betty fix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,11 +1,11 @@
 #include "search_algos.h"
 
 /**
- * linea_search - linear search though an array
+ * linear_search - linear search though an array
  * @array: ponter to start of array
  * @size: number of items in array
  * @value: value to search for
- * @Return: index of value or (-1) on failure
+ * Return: index of value or (-1) on failure
  */
 
 int linear_search(int *array, size_t size, int value)
